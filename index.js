7f781186254185bfca19d055566ea14732617448
Kami <harshimymommy@gmail.com>
2024-04-29 00:33:52 +0000

    feat: 🗃️ Just updating
    Signed-off-by: Kami <harshimymommy@gmail.com>

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const Eris = require("eris")
+const { Database } = require("bun:sqlite")
+
+const db = new Database("./mydb.sqlite");
 
 const bot = new Eris(process.env.bot_token);
 bot.on("ready", () => {
